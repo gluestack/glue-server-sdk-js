ed7eeede74015e19082d48f9cde48e24bf953ca5
Sandeep kumar <sandeepk@geekyants.com>
2023-01-18 14:05:31 +0530

    chore: add email class with send method

---
+++ src/email/interfaces/IEmail.ts
@@ -0,0 +1,28 @@
+import { Stream } from "stream";
+import { IAttachments } from "./IAttachment";
+import { IEmailResponse } from "./IEmailRespose";
+
+export interface IEmail {
+  baseUrl: string;
+
+  send(
+    emailBody: {
+      mailOptions: {
+        from: string,
+        to: string | Array<string | object>,
+        subject: string,
+        html: string | Buffer | Stream | IAttachments,
+        data: object,
+        text?: string | Buffer | Stream | IAttachments,
+        cc?: string | Array<string | object>,
+        bcc?: string | Array<string | object>,
+        attachments?: Array<IAttachments>
+      },
+      transportOptions: {
+        host: string,
+        port: number,
+        auth: object
+      }
+    }
+  ): Promise<IEmailResponse>;
+}
---
+++ src/email/interfaces/IEmailRespose.ts
@@ -0,0 +1,4 @@
+export interface IEmailResponse {
+  status: boolean;
+  message: string;
+}
